de3b72bdbc6b081af1a06159006645bb8395e83f
RemuruSama <abhinavftp98@gmail.com>
2026-06-23 08:05:26 +0530

    build: Bump version to 3.1.0 (25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.abhinav.otapulse"
         minSdk = 29
         targetSdk = 37
-        versionCode = 24
-        versionName = "3.0.9"
+        versionCode = 25
+        versionName = "3.1.0"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
